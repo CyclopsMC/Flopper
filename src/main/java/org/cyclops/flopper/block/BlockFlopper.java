8d2932c652e8ad4289fce26aa5b2e5d398752d47
Ruben Taelman <rubensworks@gmail.com>
2021-09-20 07:57:45 +0200

    Fix crash with Hopper+ mod, Closes #22

--- src/main/java/org/cyclops/flopper/block/BlockFlopper.java
+++ src/main/java/org/cyclops/flopper/block/BlockFlopper.java
@@ -12,6 +12,7 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.IHopper;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
@@ -21,8 +22,10 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
@@ -57,6 +60,22 @@ public class BlockFlopper extends BlockTile {
     public static final DirectionProperty FACING = BlockStateProperties.FACING_EXCEPT_UP;
     public static final BooleanProperty ENABLED = BlockStateProperties.ENABLED;
 
+    // Copied from HopperBlock, to avoid conflicts with other mods messing with the hopper
+    private static final VoxelShape INPUT_SHAPE = Block.makeCuboidShape(0.0D, 10.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    private static final VoxelShape MIDDLE_SHAPE = Block.makeCuboidShape(4.0D, 4.0D, 4.0D, 12.0D, 10.0D, 12.0D);
+    private static final VoxelShape INPUT_MIDDLE_SHAPE = VoxelShapes.or(MIDDLE_SHAPE, INPUT_SHAPE);
+    private static final VoxelShape BASE_SHAPE = VoxelShapes.combineAndSimplify(INPUT_MIDDLE_SHAPE, IHopper.INSIDE_BOWL_SHAPE, IBooleanFunction.ONLY_FIRST);
+    private static final VoxelShape DOWN_SHAPE = VoxelShapes.or(BASE_SHAPE, Block.makeCuboidShape(6.0D, 0.0D, 6.0D, 10.0D, 4.0D, 10.0D));
+    private static final VoxelShape EAST_SHAPE = VoxelShapes.or(BASE_SHAPE, Block.makeCuboidShape(12.0D, 4.0D, 6.0D, 16.0D, 8.0D, 10.0D));
+    private static final VoxelShape NORTH_SHAPE = VoxelShapes.or(BASE_SHAPE, Block.makeCuboidShape(6.0D, 4.0D, 0.0D, 10.0D, 8.0D, 4.0D));
+    private static final VoxelShape SOUTH_SHAPE = VoxelShapes.or(BASE_SHAPE, Block.makeCuboidShape(6.0D, 4.0D, 12.0D, 10.0D, 8.0D, 16.0D));
+    private static final VoxelShape WEST_SHAPE = VoxelShapes.or(BASE_SHAPE, Block.makeCuboidShape(0.0D, 4.0D, 6.0D, 4.0D, 8.0D, 10.0D));
+    private static final VoxelShape DOWN_RAYTRACE_SHAPE = IHopper.INSIDE_BOWL_SHAPE;
+    private static final VoxelShape EAST_RAYTRACE_SHAPE = VoxelShapes.or(IHopper.INSIDE_BOWL_SHAPE, Block.makeCuboidShape(12.0D, 8.0D, 6.0D, 16.0D, 10.0D, 10.0D));
+    private static final VoxelShape NORTH_RAYTRACE_SHAPE = VoxelShapes.or(IHopper.INSIDE_BOWL_SHAPE, Block.makeCuboidShape(6.0D, 8.0D, 0.0D, 10.0D, 10.0D, 4.0D));
+    private static final VoxelShape SOUTH_RAYTRACE_SHAPE = VoxelShapes.or(IHopper.INSIDE_BOWL_SHAPE, Block.makeCuboidShape(6.0D, 8.0D, 12.0D, 10.0D, 10.0D, 16.0D));
+    private static final VoxelShape WEST_RAYTRACE_SHAPE = VoxelShapes.or(IHopper.INSIDE_BOWL_SHAPE, Block.makeCuboidShape(0.0D, 8.0D, 6.0D, 4.0D, 10.0D, 10.0D));
+
     public BlockFlopper(Properties properties, Supplier<CyclopsTileEntity> tileEntitySupplier) {
         super(properties, tileEntitySupplier);
         MinecraftForge.EVENT_BUS.register(this);
@@ -72,12 +91,40 @@ public class BlockFlopper extends BlockTile {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return Blocks.HOPPER.getShape(state, worldIn, pos, context);
+        // Copied from HopperBlock, to avoid conflicts with other mods messing with the hopper
+        switch((Direction)state.get(FACING)) {
+            case DOWN:
+                return DOWN_SHAPE;
+            case NORTH:
+                return NORTH_SHAPE;
+            case SOUTH:
+                return SOUTH_SHAPE;
+            case WEST:
+                return WEST_SHAPE;
+            case EAST:
+                return EAST_SHAPE;
+            default:
+                return BASE_SHAPE;
+        }
     }
 
     @Override
     public VoxelShape getRaytraceShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
-        return Blocks.HOPPER.getRaytraceShape(state, worldIn, pos);
+        // Copied from HopperBlock, to avoid conflicts with other mods messing with the hopper
+        switch((Direction)state.get(FACING)) {
+            case DOWN:
+                return DOWN_RAYTRACE_SHAPE;
+            case NORTH:
+                return NORTH_RAYTRACE_SHAPE;
+            case SOUTH:
+                return SOUTH_RAYTRACE_SHAPE;
+            case WEST:
+                return WEST_RAYTRACE_SHAPE;
+            case EAST:
+                return EAST_RAYTRACE_SHAPE;
+            default:
+                return IHopper.INSIDE_BOWL_SHAPE;
+        }
     }
 
     @SuppressWarnings("unchecked")
